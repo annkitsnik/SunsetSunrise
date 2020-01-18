e17239cf2d82848f4403cd2ba3852800909342a7
Ann Kitsnik <annkitsnik@hot.ee>
2020-01-18 12:19:32 +0200

    first commit

---
+++ SunsetSunrise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace SunsetSunrise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userCategoryUrl = "https://api.sunrise-sunset.org/json?lat=59.436962&lng=24.753574&date=today";
+
+            HttpWebRequest request = (HttpWebRequest)FtpWebRequest.Create(userCategoryUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader= new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine(sunMode.Status);
+                Console.WriteLine($"Sunrise{sunMode.Results.Sunrise}");
+                Console.WriteLine($"Sunset{sunMode.Results.Sunset}");
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SunsetSunrise/SunMode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunsetSunrise
+{
+    class SunMode
+    {
+        public string Status { get; set; }
+        public Results Results { get; set; }
+
+
+    }
+
+    public class Results
+    {
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+    }
+
+
+}
